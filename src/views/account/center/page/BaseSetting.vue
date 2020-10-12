e0668373a2de9aac18bd5732424126450f220dcf
guqing <1484563614@qq.com>
2020-10-12 21:09:24 +0800

    feat: 新增修改个人信息

--- src/views/account/center/page/BaseSetting.vue
+++ src/views/account/center/page/BaseSetting.vue
@@ -2,26 +2,34 @@
   <div class="account-settings-info-view">
     <a-row :gutter="16">
       <a-col :md="24" :lg="16">
-        <a-form layout="vertical">
-          <a-form-item label="昵称" :required="true">
-            <a-input placeholder="给自己起个名字" v-model="user.nickname" />
+        <a-form layout="vertical" :form="form">
+          <a-form-item label="昵称">
+            <a-input
+              v-decorator="['nickname', { rules: [{ required: true, message: '昵称不能为空!' }] }]"
+              placeholder="给自己起个名字"
+            />
           </a-form-item>
           <a-form-item label="Bio">
-            <a-textarea rows="4" placeholder="You are not alone." v-model="user.description" />
+            <a-textarea
+              v-decorator="['description', { rules: [{ max: 144, message: '个人说明不能超过144字符!' }] }]"
+              rows="4"
+              placeholder="You are not alone."
+            />
           </a-form-item>
 
-          <a-form-item label="电子邮件" :required="false">
-            <a-input placeholder="exp@admin.com" v-model="user.email" />
+          <a-form-item label="电子邮件">
+            <a-input
+              v-decorator="['email', { rules: [{ type: 'email', message: '邮箱地址格式不正确!' }] }]"
+              placeholder="exp@admin.com"
+            />
           </a-form-item>
 
-          <a-form-item label="手机号码" :required="false">
-            <a-input placeholder="填写一个手机号" v-model="user.telephone" />
+          <a-form-item label="手机号码">
+            <a-input v-decorator="['mobile']" placeholder="填写一个手机号" />
           </a-form-item>
 
           <a-form-item>
-            <a-button type="primary" @click="handleUpdateUserInfo">
-              保存
-            </a-button>
+            <a-button type="primary" @click="handleUpdateUserInfo"> 保存 </a-button>
           </a-form-item>
         </a-form>
       </a-col>
@@ -32,13 +40,16 @@
 <script>
 import userApi from '@/api/user'
 import { mapActions } from 'vuex'
+import pick from 'lodash.pick'
 
 export default {
   name: 'BaseSetting',
   data () {
     return {
-      user: {}
     }
+  },
+  beforeCreate () {
+    this.form = this.$form.createForm(this, { name: 'user_profile' })
   },
   mounted () {
     this.loadUser()
@@ -48,26 +59,31 @@ export default {
 
     loadUser () {
       this.GetInfo().then(res => {
-        this.user = res
+        this.form.setFieldsValue(pick(res, 'nickname', 'email', 'mobile', 'description'))
       })
     },
 
-    handleUpdateUserInfo () {
-      if (this.user.nickname === '') {
-        this.$message.warning('昵称不能为空')
-        return
-      }
-      if (this.user.description.length > 144) {
-        this.$message.warning('个人说明不能超过144个字符')
-        return
-      }
-      userApi.updateUserInfo(this.user).then(res => {
-        this.$message.success('保存成功')
-        this.loadUser()
-        // 更新vuex状态
-        this.GetInfo()
-      }).catch(err => {
-        this.$message.error(`更新用户信息出错,error: ${err.message}`)
+    handleUpdateUserInfo (e) {
+      // if (this.user.nickname === '') {
+      //   this.$message.warning('昵称不能为空')
+      //   return
+      // }
+      // if (this.user.description.length > 144) {
+      //   this.$message.warning('个人说明不能超过144个字符')
+      //   return
+      // }
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          userApi.updateProfile(values).then(res => {
+            this.$message.success('保存成功')
+            this.loadUser()
+            // 更新vuex状态
+            this.GetInfo()
+          }).catch(err => {
+            this.$message.error(`更新用户信息出错,error: ${err.message}`)
+          })
+        }
       })
     }
   }
